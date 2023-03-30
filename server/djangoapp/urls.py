86810dc2a51b5ccc61c35c9edc14f9e55097eb38
idoamihai <idoamihai@gmail.com>
2023-03-30 16:54:06 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,26 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about', view=views.contact, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='registration', view=views.contact, name='registration'),
 
     # path for login
+    path(route='login', view=views.contact, name='login'),
 
     # path for logout
+    path(route='logout', view=views.contact, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
+    path(route='', view=views.get_dealerships, name='index'),
+    
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
