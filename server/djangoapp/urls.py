fd9f2b0ae88ead650b14337c9bb43fcf4e3b977f
ido amihai <idoamihai@gmail.com>
2023-04-07 06:21:23 -0400

    week1 fixes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,25 +10,20 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about', view=views.contact, name='about'),
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-    path(route='registration', view=views.contact, name='registration'),
 
     # path for login
-    path(route='login', view=views.contact, name='login'),
 
     # path for logout
-    path(route='logout', view=views.contact, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
-    path(route='', view=views.get_dealerships, name='index'),
     
     # path for add a review view
 
