7b0ba985551188f2ca5ec95a7c05ed5a0d41f2a0
ido <ido>
2023-04-30 08:41:22 -0400

    week 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -76,7 +76,7 @@ class CarDealer:
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
 
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id_):
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
@@ -86,7 +86,7 @@ class DealerReview:
         self.car_model = car_model
         self.car_year = car_year
         self.sentiment = sentiment
-        self.id = id_
+        self.id = id
 
     def __str__(self):
         return f"Review by: {self.name}\n Review:{self.review}"
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -100,20 +100,20 @@ def store_review(url, payload):
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-# def analyze_review_sentiments(text):
-#     url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1d6d2a23-a3c3-4ecb-bb1d-1d3eebb6a8c6/v1/analyze?version=2021-03-25"
-#     api_key = "ul4LUhas5N_IUWIGV1wfauUZ-8W1f9JfxbOgM_2ve79j"
-#     params = {
-#         "text": text,
-#         "features": {
-#             "sentiment": {
-#             }
-#         },
-#         "language": "en"
-#     }
-#     # params["return_analyzed_text"] = kwargs["return_analyzed_text"]
-#     response = requests.post(url, json=params, headers={'Content-Type': 'application/json'},
-#                                     auth=('apikey', api_key))
-#     return response.json()["sentiment"]["document"]["label"]
+def analyze_review_sentiments(text):
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/4a593f46-c22a-4375-b2ec-98bf324e41cd/v1/analyze?version=2021-03-25"
+    api_key = "V28Zh6Aa6zFS6yoTNM4TZKxLsp42vnTyCZ5Uyn9FlriJ"
+    params = {
+        "text": str(text),
+        "features": {
+            "sentiment": {
+            }
+        },
+        "language": "en"
+    }
+    # params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+    response = requests.post(url, json=params, headers={'Content-Type': 'application/json'},
+                                    auth=('apikey', api_key))
+    return response.json()["sentiment"]["document"]["label"]
 
 
